2e64aa3b51fe332630afd66e66f55e4cb0d15ec3
James <jamestron@gmail.com>
2018-09-27 21:26:15 +1200

    added folders

---
+++ Assets/Scripts/CardSequencer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardSequencer : MonoBehaviour {
+
+
+    [SerializeField]
+    private GameObject toyObject;
+
+
+	void Start ()
+    {
+		
+	}
+	
+	void Update ()
+    {
+		
+	}
+
+
+
+
+}
